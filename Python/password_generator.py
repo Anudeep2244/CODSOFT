5b74e9823847dbbfcf6449ceeef14ca0cbac660f
anudeep <anudepbetageri22@gmail.com>
2024-08-09 18:09:46 +0530

    password_generator.py

--- Python/password_generator.py
+++ Python/password_generator.py
@@ -5,7 +5,7 @@ num = '1234567890'
 special = "!@#$%^&*()_-+={[]}|;:',.<>?/"
 choices = upper+lower+num+special
 while True:
-    length = input("\nEnter your desired length for your password : ")
+    length = input("\nEnter your desired length for your password : ").strip()
     if length.isdigit():
         length = int(length)
         if length >= 1:
