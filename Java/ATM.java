e4125ca16190ca12e9a991d64ff78a162546e1c5
anudeep <anudepbetageri22@gmail.com>
2024-08-26 15:33:24 +0530

    Add ATM code

---
+++ Java/ATM.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class ATMSystem {
+    private float balance;
+
+    public ATMSystem(){
+        balance = 0;
+    }
+
+    public void deposit(float amount){
+        balance += amount;
+        System.out.println("Deposit successful.");
+    }
+
+    public void withdraw(float amount){
+        if (amount > balance){
+            System.out.println("Insufficient Balance.");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful.");
+        }
+    }
+
+    public void showBalance(){
+        System.out.println("Balance: " + balance);
+    }
+}
+
+public class ATM {
+    public static void main(String[] args) {
+        ATMSystem user = new ATMSystem();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        float amount;
+        do { 
+            System.out.println("\n1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your option: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1: 
+                    System.out.print("Enter the amount you want to deposit: ");
+                    amount = scanner.nextFloat();
+                    if (amount > 0){
+                        user.deposit(amount);
+                    } else {
+                        System.out.println("Invalid amount.");
+                    }
+                    break;
+                
+                case 2: 
+                    System.out.print("Enter the amount you want to withdraw: ");
+                    amount = scanner.nextFloat();
+                    if (amount > 0) {
+                        user.withdraw(amount);
+                    } else {
+                        System.out.println("Invalid amount.");
+                    }
+                    break;
+                
+                case 3: 
+                    user.showBalance();
+                    break;
+
+                case 4: 
+                    System.out.println("Thank you for using the ATM!");
+                    break;
+                
+                default: 
+                    System.out.println("Invalid input, please try again.");
+            }
+        } while (choice != 4);
+        scanner.close();
+    }
+}
---
+++ Java/Guess_num.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Guess_num {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int total_rounds = 0;
+        int total_attempts = 0;
+        int wins = 0;
+        boolean play_again = true;
+        final int max_attempts = 10;
+
+        System.out.println("*** Number Guessing Game ***");
+        System.out.println("Let's see if you can guess the number.");
+        System.out.println("You'll only get " + max_attempts + " attempts to guess the number.");
+
+
+        while (play_again) {
+            int num_to_guess = random.nextInt(1,101);
+            int attempt_number = 1;
+            boolean guess = false;
+
+            while (attempt_number <= max_attempts &&  !guess) { 
+                System.out.print("\nEnter your guess : ");
+                int user_guess = scanner.nextInt();
+
+                if (user_guess > num_to_guess) {
+                    System.out.println("It's high! Try again.");
+                }
+                else if (user_guess < num_to_guess) {
+                    System.out.println("It's low! Try again.");
+                } 
+                else {
+                    System.out.println("Yeahh! You guessed it right in just " + attempt_number + " attempts.");
+                    wins++;
+                    guess = true;
+                    break;
+                }
+                attempt_number++;
+            }
+            total_rounds++;
+            total_attempts += attempt_number;
+
+            if (!guess) {
+                System.out.println("Sorry, you've used all your attempts. The number was: " + num_to_guess);
+            }
+
+            System.out.println("\nRound summary: ");
+            System.out.println("Total rounds played: " + total_rounds);
+            System.out.println("Rounds won: " + wins);
+            System.out.println("Total attempts so far: " + total_attempts);
+
+            System.out.println("\nYou wanna play again ? [yes / no] : ");
+            String ch = scanner.next().toLowerCase().strip();
+            
+            play_again = ch.equals("yes");  
+        }
+        System.out.println("\nThankyou for playing.");
+        System.out.println("Total rounds played : " + total_rounds);
+        System.out.println("Total attempts : " + total_attempts);
+        System.out.println("Rounds won: " + wins);
+
+        scanner.close();
+    }
+}
